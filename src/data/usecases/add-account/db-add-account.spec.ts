a816f3a627d9217a151fd8b98c359a5407123c0a
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2020-07-20 16:44:48 -0300

    refactor: change function return

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -6,7 +6,7 @@ interface SutTypes {
   encrypterStub: Encrypter
 }
 
-const makeSut = (): any => {
+const makeSut = (): SutTypes => {
   class EncrypterStub {
     async encrypt (value: string): Promise<string> {
       return new Promise(resolve => resolve('hashed_password'))
